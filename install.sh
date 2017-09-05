982d2eb170b0f765705ae2fdeabc861586ca54ac
rubenwardy <rw@rubenwardy.com>
2017-09-05 19:51:14 +0000

    Add Lua5.1

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 apt-get update &&
-DEBIAN_FRONTEND=noninteractive apt-get install cmake build-essential p7zip-full wget -y &&
+DEBIAN_FRONTEND=noninteractive apt-get install cmake build-essential p7zip-full wget lua5.1 liblua5.1-dev -y &&
 DEBIAN_FRONTEND=noninteractive apt-get build-dep libsfml-dev -y &&
 
 
